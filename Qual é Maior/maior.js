64cf0dc441cd1883ca71459f951512ebb9a6603d
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-04 18:36:23 -0300

    condições
    Sempre ver se as condições estão declaradas,

--- Qual é Maior/maior.js
+++ Qual é Maior/maior.js
@@ -2,11 +2,12 @@ function calcular() {
     var num1 = Number(window.prompt(`Digite um número:`))
     var num2 = Number(window.prompt(`Digite outro número:`))
     var res = document.getElementById(`resultado`) 
-    var maior //Nao precis
-    if (num1 > num2) {
+    
+    if (num1 > num2) { // Colocar a condições no if
+        res.innerHTML = `Analisando os valores ${num1} e ${num2}, o maior valor é ${num1} `
+    } else if (num1 < num2){ // Colocar a condições no else if para que o (else consigar se validado)
         res.innerHTML = `Analisando os valores ${num1} e ${num2}, o maior valor é ${num2} `
-    } else {
-        maior = num2
-    }
-    res.innerHTML=`${maior}`
+    } else { // Acho que nao precisa ter um condições aqui...
+        res.innerHTML = `Analisando os valores ${num1} e ${num2}, ambos os valores são igual `
+    } 
 }
