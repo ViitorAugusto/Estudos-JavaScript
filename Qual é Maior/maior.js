ba954140d3893225caf7c0b659ddf6605271678c
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-04 19:09:20 -0300

    CSS

--- Qual é Maior/maior.js
+++ Qual é Maior/maior.js
@@ -4,10 +4,10 @@ function calcular() {
     var res = document.getElementById(`resultado`) 
     
     if (num1 > num2) { // Colocar a condições no if
-        res.innerHTML = `Analisando os valores ${num1} e ${num2}, o maior valor é ${num1} `
+        res.innerHTML = `<p>Analisando os valores<mark> ${num1}</mark> e <mark>${num2}</mark>, o maior valor é <mark>${num1}</mark></p> `
     } else if (num1 < num2){ // Colocar a condições no else if para que o (else consigar se validado)
-        res.innerHTML = `Analisando os valores ${num1} e ${num2}, o maior valor é ${num2} `
+        res.innerHTML =  `<p>Analisando os valores<mark> ${num1}</mark> e <mark>${num2}</mark>, o maior valor é <mark>${num2}</mark></p> `
     } else { // Acho que nao precisa ter um condições aqui...
-        res.innerHTML = `Analisando os valores ${num1} e ${num2}, ambos os valores são igual `
+        res.innerHTML = `<p>Analisando os valores <mark>${num1}</mark> e <mark> ${num2}</mark>, ambos os valores são igual</p> `
     } 
 }
