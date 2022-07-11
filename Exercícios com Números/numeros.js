aea2bff96b0811cd170c4b6450db3dbf20bdc815
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-11 17:07:32 -0300

    Update numeros.js

--- Exercícios com Números/numeros.js
+++ Exercícios com Números/numeros.js
@@ -1,8 +1,11 @@
 const numero = Number(prompt(`Digite um número`))
 const resultado= document.getElementById(`resultado`)
-document.getElementById(`numero-titulo`)
+const titulo = document.getElementById(`numero-titulo`)
 
-resultado.innerHTML = `seu ${numero} foi`
+titulo.innerHTML =`${numero}`
+resultado.innerHTML += `<p>Raiz quadrada: <strong><mark>${numero ** 0.5}</mark></strong></p> ` // para calcular a raiz quadrada 
+resultado.innerHTML += `<p>${numero} é inteiro :<strong><mark> ${Number.isInteger(numero)}</mark></strong></p>`
 
 
 
+<strong><mark></mark></strong>
