50beba65dfe3646720fd42aa0360bb9d5c3ed03e
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-11 17:14:23 -0300

    Update numeros.js

--- Exercícios com Números/numeros.js
+++ Exercícios com Números/numeros.js
@@ -1,11 +1,15 @@
 const numero = Number(prompt(`Digite um número`))
 const resultado= document.getElementById(`resultado`)
 const titulo = document.getElementById(`numero-titulo`)
-
 titulo.innerHTML =`${numero}`
-resultado.innerHTML += `<p>Raiz quadrada: <strong><mark>${numero ** 0.5}</mark></strong></p> ` // para calcular a raiz quadrada 
-resultado.innerHTML += `<p>${numero} é inteiro :<strong><mark> ${Number.isInteger(numero)}</mark></strong></p>`
 
+resultado.innerHTML += `<p>Raiz quadrada: <strong><mark>${numero ** 0.5}</mark></strong></p> ` // para calcular a raiz quadrada. 
 
+resultado.innerHTML += `<p>${numero} é inteiro :<strong><mark> ${Number.isInteger(numero)}</mark></strong></p>` // .isInteger numero inteiro.
 
-<strong><mark></mark></strong>
+resultado.innerHTML += `<p>Arredondando para baixo :<strong><mark> ${Math.floor(numero)}</mark></strong>.</p>` // Math.floor(var) para baixo
+
+resultado.innerHTML += `<p>Arredondando para cima :<strong><mark> ${Math.ceil(numero)}</mark></strong>.</p>` // Math.ceil(var) para cima
+
+resultado.innerHTML += `<p>Com duas casas decimais :<strong><mark> ${numero.toFixed (2)}</mark></strong>.</p>` // Mudando as casas decimais com .toFixed(casas (2))
+
