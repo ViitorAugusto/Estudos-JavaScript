72fbc42f14d27d2510f21594777e6775c5ae632d
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-12 18:00:12 -0300

    Update formu.js

--- Formulário/formu.js
+++ Formulário/formu.js
@@ -2,26 +2,27 @@
     const form = document.querySelector(`.form`)
     const resultado = document.querySelector(`.resultado`)
 
-    const pessoas = []
+    const pessoas = []  // uma array vazia
 
     function recebeEventoForm (evento) {
-        evento.preventDefault()
+        evento.preventDefault()  // .preventDefault => desativa o browser de ser atualizado 
         const nome = form.querySelector(`.nome`)
         const sobrenome = form.querySelector(`.sobrenome`)
         const peso = form.querySelector(`.peso`)
         const altura =  form.querySelector(`.altura`)
         
-        pessoas.push ({
+        pessoas.push ({ // object , array estou criando com (.push) 
             nome: nome.value,
-            sobrenome: sobrenome.value,
+            sobrenome: sobrenome.value, // usando .value pois quero apenas o valor do input
             peso: peso.value,
             altura: altura.value
         })
-         console.log(pessoas)
+         console.log(pessoas) // pra ver salva as array
         
         resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
     }
     
-    form.addEventListener(`submit`, recebeEventoForm)
+    form.addEventListener(`submit`, recebeEventoForm) /*addEventListener, ainda não sei bem pra que serve
+     mas aparentemente usado pra criar um evento.*/
  }
- meuFormulario ()
+ meuFormulario () // executando minha function
