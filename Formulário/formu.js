7a2014889108c8b077b4a0ddfafa130992301979
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-12 17:44:56 -0300

    Update formu.js

--- Formulário/formu.js
+++ Formulário/formu.js
@@ -1,6 +1,6 @@
  function meuFormulario ()  {
     const form = document.querySelector(`.form`)
-    const resultado = document.querySelector(`resultado`)
+    const resultado = document.querySelector(`.resultado`)
 
     function recebeEventoForm (evento) {
         evento.preventDefault()
@@ -8,9 +8,9 @@
         const sobrenome = form.querySelector(`.sobrenome`)
         const peso = form.querySelector(`.peso`)
         const altura =  form.querySelector(`.altura`)
-
+        resultado.innerHTML = `texte ${nome.value}`
     }
-
+    
     form.addEventListener(`submit`, recebeEventoForm)
  }
  meuFormulario ()
