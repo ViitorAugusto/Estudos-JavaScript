de1f32f86e5e06cdcfe2d2671c5eec06ff9dff1d
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-13 18:43:53 -0300

    IMC

--- Tabela IMC/imc.js
+++ Tabela IMC/imc.js
@@ -6,8 +6,18 @@ function calcular() {
     let msg 
     if (imc < 18.5) {
         msg = `Abaixo do peso`
-    } else if ( imc)
+    } else if ( imc >= 18.5 && imc <= 24.9 ) {
+        msg = `Peso normal`
+    } else if (imc >= 25 && imc <= 29.9 ) {
+        msg = `Sobrepeso`
+    } else if ( imc >= 30 && imc <= 34.9) {
+        msg = `Obesidade grau 1`
+    } else if ( imc >= 35 && imc <= 39.9) {
+        msg = `Obesidade grau 2`
+    } else  {
+        msg= `Obesidade grau 3`
+    }
 
-    resultado.innerHTML =`${imc}`
+    resultado.innerHTML =` Seu IMC é :${imc.toFixed(2)}, ${msg}`
 
 }
