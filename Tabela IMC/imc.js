18c02facf42586cf8724cb8399f244b19e47aa25
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-15 16:57:34 -0300

    Update imc.js

--- Tabela IMC/imc.js
+++ Tabela IMC/imc.js
@@ -3,17 +3,27 @@
     form.addEventListener(`submit`, function (e) { // Esse (e) e uma abreviação de .evento
 
         e.preventDefault()
-        console.log(`evento previnido.`)
-        setResultado (`Ola Mundo`)
+        
+        const inputPeso = e.target.querySelector(`#peso`) //e.target vai me mostrar qual elemento foi clicado na página.
+        const inputAltura = e.target.querySelector(`#altura`) // porém nao serve para pegar o valor no input só uma outra forma de pegar esse input poderia usar tambem document.querySelector.
+        
+       
     } );  // Essa parte do códico para o enveio desse Formulário.
-    
-    function setResultado (msg) {
-        const resul = document.getElementById(`resul`)
-        resul.innerHTML = `` // Essa function vai mostrar meu resultado em uma div.function setResultado Vou deixar em branco pra toda a vez que eu charmar ela ficar vazio
+
+    function criarP (){
         const p = document.createElement('p') // Estou criando um parágrafo
         p.classList.add(`paragrafo-resultado`) //Criei uma class com esse comando
         p.innerHTML = `Qual quer coisa` // colocar html dentro do parágrafi
         resul.appendChild(p) // inclui um filho (elemento) nessa div no caso (p)
+    }
+    
+    function setResultado (msg) {
+        const resul = document.getElementById(`resul`)
+        resul.innerHTML = `` // Essa function vai mostrar meu resultado em uma div.function setResultado Vou deixar em branco pra toda a vez que eu charmar ela ficar vazio
+        const p = criarP () // colocando a function pra rodar no resultado
+        
+       
+        
     }
 
 
