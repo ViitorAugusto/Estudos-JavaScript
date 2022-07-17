288b31be5dcdd2cea4facfd76eaa302c70a6d113
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-17 16:01:56 -0300

    Update switch-case.js

--- Switch-Case/switch-case.js
+++ Switch-Case/switch-case.js
@@ -3,4 +3,12 @@ const elementos = [
     {tag: 'div' , texto: `Frase 2`},
     {tag: 'section' , texto: `Frase 3`},
     {tag: 'footer' , texto: `Frase 4`},
-]
+];
+
+const container = document.querySelector(`.container`)
+const div = document.createElement(`div`); // Criada uma Div pra colocar todos os elementos do meu array
+
+for (let i = 0; < elementos.length ; i++ ) {
+
+}
+
