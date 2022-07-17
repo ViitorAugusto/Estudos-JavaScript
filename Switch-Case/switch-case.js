f76e4e1fc3dc8c69206ca8b917054b6b770e55e0
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-17 16:12:54 -0300

    Update switch-case.js

--- Switch-Case/switch-case.js
+++ Switch-Case/switch-case.js
@@ -3,12 +3,17 @@ const elementos = [
     {tag: 'div' , texto: `Frase 2`},
     {tag: 'section' , texto: `Frase 3`},
     {tag: 'footer' , texto: `Frase 4`},
+    {tag: 'main' , texto: `Frase 5`}
 ];
 
 const container = document.querySelector(`.container`)
 const div = document.createElement(`div`); // Criada uma Div pra colocar todos os elementos do meu array
 
 for (let i = 0; i < elementos.length ; i++ ) {
-    console.log(elementos[i].tag)
+    let { tag, texto} = elementos[i]
+    let tagCriada = document.createElement(tag) // colocar a tag se eu colocar (p) vai criar 4 parágrafos.
+    tagCriada.innerText = texto
+    div.appendChild(tagCriada) // Add as tag dentro dessa div.
 }
+container.appendChild(div) // Usando a Div criada aqui já com os elementos.
 
