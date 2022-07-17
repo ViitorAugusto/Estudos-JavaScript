7df693e074a4cc319fabd3ae700702af04021359
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-17 15:58:24 -0300

    Switch-Case

---
+++ Switch-Case/switch-case.js
@@ -0,0 +1,6 @@
+const elementos = [
+    {tag: 'p' , texto: `Qualquer texto que voce quiser.`},
+    {tag: 'div' , texto: `Frase 2`},
+    {tag: 'section' , texto: `Frase 3`},
+    {tag: 'footer' , texto: `Frase 4`},
+]
