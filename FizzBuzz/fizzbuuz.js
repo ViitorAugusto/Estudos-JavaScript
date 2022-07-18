b92e229c24ebc6867544fe4f6e869b4676876d2e
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-18 16:58:25 -0300

    Update fizzbuuz.js

--- FizzBuzz/fizzbuuz.js
+++ FizzBuzz/fizzbuuz.js
@@ -6,3 +6,12 @@
 // Número NÃO é divisível por 3 e 5 = Retorne o próprio número
 // Checar se o número é realmente um número = Retorna o próprio número
 // Use a funcão com números de 0 a 100.
+function fizzbuzz (numero) {
+    // !== Diferente ou ig
+    if (typeof numero !==)
+    if (numero % 3 === 0) return `Fizz`
+    if (numero % 5 === 0) return `Buzz`
+    if (numero % 3 === 0) return `Fizz`
+
+}
+
