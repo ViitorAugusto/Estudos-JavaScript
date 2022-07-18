448258730434bff6309e06de3bad0b15ca67c248
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-18 16:25:51 -0300

    Create index.js

---
+++ Qual é Maior/Retorne o maior/index.js
@@ -0,0 +1,18 @@
+function max (x ,y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y
+    }
+}
+const maior = max (15, 11) 
+console.log(maior) //Posso chamar a function direto.
+// console.log(max(15, 11)); 
+
+function max (x ,y) {
+    if (x > y) {
+        return x;
+    }  
+    return y
+    
+}
