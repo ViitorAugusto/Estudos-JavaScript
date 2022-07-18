8f28c105e9ebc58610d9d7e4d77d55b6bee384ca
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-18 16:49:06 -0300

    Create fizzbuuz.js

---
+++ FizzBuzz/fizzbuuz.js
@@ -0,0 +1,8 @@
+// Escreva uma funcão que recebe um número e
+// retorne o seguinte :
+// Número é divivível por 3 = Fizz
+// Número é divivível por 5 = Buzz
+// Número é divivível por 3  e 5 = FizzBuzz
+// Número NÃO é divisível por 3 e 5 = Retorne o próprio número
+// Checar se o número é realmente um número = Retorna o próprio número
+// Use a funcão com números de 0 a 100.
