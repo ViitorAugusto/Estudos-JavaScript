ad8f86f00ece28a8ec0429da2da00ec497346897
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-19 16:54:17 -0300

    Timer

---
+++ Meu timer/timer.js
@@ -0,0 +1,19 @@
+const relogio =document.querySelector(`.relogio`)
+const iniciar = document.querySelector(`.iniciar`)
+const pausar = document.querySelector(`.pausar`)
+const zerar = document.querySelector(`.zerar`)
+
+iniciar.addEventListener(`click`, function(event)
+{ alert(`cliquei no inciar`)
+
+}) 
+
+pausar.addEventListener(`pausar`, function(event)
+{ alert(`cliquei no pausar`)
+
+}) 
+
+zerar.addEventListener(`zerar`, function(event)
+{ alert(`cliquei no zerar`)
+
+}) 
