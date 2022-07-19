682d2f5a73521f1f52dbd890508f704c8c9e1959
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-19 18:33:59 -0300

    Update timer.js

--- Meu timer/timer.js
+++ Meu timer/timer.js
@@ -1,3 +1,4 @@
+function relogio() {
 function criarHoraDosSegundos (segundos) {
     const data = new Date (segundos * 1000) // Multiplicar por 1000 para obter o milésimo de segundos
     return data.toLocaleTimeString(`pt-BR`,
@@ -11,7 +12,7 @@ const relogio = document.querySelector(`.relogio`)
 const iniciar = document.querySelector(`.iniciar`)
 const pausar = document.querySelector(`.pausar`)
 const zerar = document.querySelector(`.zerar`)
-let segundos = 0; // 
+let segundos = 0; 
 let timer 
 
 function iniciaRelogio(){
@@ -38,3 +39,14 @@ zerar.addEventListener(`click`, function(event){
     segundos = 0
     relogio.classList.remove(`pausado`) 
 }) 
+}
+relogio()
+// Modelo com e.target 
+//document.addEventListener('click', function(e){
+//    const el = e.target //Novamente target vou pegar o click no document
+//    if (el.classList.contains(`zerar`))
+//    clearInterval(timer)
+//    relogio.innerHTML = `00:00:00`
+//    segundos = 0
+//    relogio.classList.remove(`pausado`) 
+// })
