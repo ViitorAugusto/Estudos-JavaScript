a53d8b8c83979e88238f3004f02f60a8a616bf0b
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-19 17:16:29 -0300

    Update timer.js

--- Meu timer/timer.js
+++ Meu timer/timer.js
@@ -1,19 +1,35 @@
-const relogio =document.querySelector(`.relogio`)
+function criarHoraDosSegundos (segundos) {
+    const data = new Date (segundos * 1000) // Multiplicar por 1000 para obter o milésimo de segundos
+    return data.toLocaleTimeString(`pt-BR`,
+    {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
+
+const relogio = document.querySelector(`.relogio`)
 const iniciar = document.querySelector(`.iniciar`)
 const pausar = document.querySelector(`.pausar`)
 const zerar = document.querySelector(`.zerar`)
+let segundos = 0;
 
-iniciar.addEventListener(`click`, function(event)
-{ alert(`cliquei no inciar`)
+function iniciaRelogio(){
+    const timer = setInterval(() => {
+        segundos++;
+        relogio.innerHTML= segundos
+    }, 1000);
+}
 
+iniciar.addEventListener(`click`, function(event){ 
+    iniciaRelogio()
 }) 
 
-pausar.addEventListener(`pausar`, function(event)
+pausar.addEventListener(`click`, function(event)
 { alert(`cliquei no pausar`)
 
 }) 
 
-zerar.addEventListener(`zerar`, function(event)
+zerar.addEventListener(`click`, function(event)
 { alert(`cliquei no zerar`)
 
 }) 
