a4c011094a2cc164d2d4d9c0623d668666e7a6fc
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-20 17:44:50 -0300

    Update addtags.js

--- Interagindo com o sistema/Interagindo com CSS/addtags.js
+++ Interagindo com o sistema/Interagindo com CSS/addtags.js
@@ -6,4 +6,8 @@ const estilosBody = getComputedStyle(document.body)
 const backgroundColorBody = estilosBody.backgroundColor
 console.log(backgroundColorBody)
 
-//
+for (let p of ps) {
+    p.style.backgroundColor = backgroundColorBody
+    p.style.color = `white`
+}
+
