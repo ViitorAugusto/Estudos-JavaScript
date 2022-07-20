ec6ef6e33de046639b72fe18e319a6eb17ded16e
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-20 17:38:56 -0300

    Estilos

--- Interagindo com o sistema/Interagindo com CSS/addtags.js
+++ Interagindo com o sistema/Interagindo com CSS/addtags.js
@@ -1,2 +1,9 @@
 const paragrafos = document.querySelector('.paragrafos')
 const ps = document.querySelectorAll(`p`)
+// Pegar os estilos do browser
+
+const estilosBody = getComputedStyle(document.body)
+const backgroundColorBody = estilosBody.backgroundColor
+console.log(backgroundColorBody)
+
+//
