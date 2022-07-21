a61f468eaf9f020d76df8e508e4c0ca6127ba126
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-21 07:16:37 -0300

    Formulário

--- Formulário/Formulário 2.0/formulario2.js
+++ Formulário/Formulário 2.0/formulario2.js
@@ -1,11 +1,17 @@
 function meuForm () {
-    const form = document.querySelector(`.formulario`)
+    const form = document.querySelector(`.form`)
 
-    form.onsubmit = function (evento) {
-        evento.preventDefault();
-        alert(1)
-        console.log(`Foi enviado`)
+    //form.onsubmit = function (evento) {
+    //    evento.preventDefault();
+    //    alert(1)
+    //    console.log(`Foi enviado`)
+    //}
+    let contador = 1;
+    function recebeEventoForm (evento) {
+        evento.preventDefautl();
+        console.log(`form nao foi enviado ${contador}`);
+        contador++;
     }
-
+    form.addEventListener(`submit`, recebeEventoForm)
 }
 meuForm();
