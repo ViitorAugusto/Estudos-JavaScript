1684aaccdca7927d60aa6d70002c6ee377ebda0b
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-21 07:18:07 -0300

    Update formulario2.js

--- Formulário/Formulário 2.0/formulario2.js
+++ Formulário/Formulário 2.0/formulario2.js
@@ -1,16 +1,9 @@
 function meuForm () {
     const form = document.querySelector(`.form`)
 
-    //form.onsubmit = function (evento) {
-    //    evento.preventDefault();
-    //    alert(1)
-    //    console.log(`Foi enviado`)
-    //}
-    let contador = 1;
+   
     function recebeEventoForm (evento) {
-        evento.preventDefautl();
-        console.log(`form nao foi enviado ${contador}`);
-        contador++;
+       
     }
     form.addEventListener(`submit`, recebeEventoForm)
 }
