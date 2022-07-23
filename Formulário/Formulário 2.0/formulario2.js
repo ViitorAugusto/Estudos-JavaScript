df4b3a70b4960fcff7b8496dfe4442cca62ecbf9
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-23 15:16:18 -0300

    Update formulario2.js

--- Formulário/Formulário 2.0/formulario2.js
+++ Formulário/Formulário 2.0/formulario2.js
@@ -1,10 +1,9 @@
-function meuForm () {
-    const form = document.querySelector(`.form`)
+const form = document.querySelector(`.form`)
 
-   
-    function recebeEventoForm (evento) {
-       
-    }
-    form.addEventListener(`submit`, recebeEventoForm)
-}
-meuForm();
+    form.addEventListener(`submit`, function (e){
+        e.preventDefault()
+
+        const inputNome = e.target.querySelector(`#name`)
+        console.log(inputNome)
+
+    })
