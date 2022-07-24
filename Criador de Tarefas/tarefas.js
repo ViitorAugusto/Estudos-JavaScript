3689d9d1130a5d14d5d7d2014d2c411c56fe5e9c
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-07-24 17:02:43 -0300

    Update tarefas.js

--- Criador de Tarefas/tarefas.js
+++ Criador de Tarefas/tarefas.js
@@ -2,6 +2,29 @@ const inputTarefas = document.querySelector(`.input-tarefas`);
 const btnTarefa = document.querySelector(`.btn-tarefa`);
 const tarefa = document.querySelector(`.tarefa`);
 
+/* ------------------ criaLi ------------------*/
+function criaLi () {
+    let li = document.createElement(`li`);
+    return li;
+}
+
+
+/* ---------------- Fim criaLi ----------------*/
+
+/* ---------------- criaTarefa ----------------*/
+
+
+function criaTarefa (textoInput) {
+    const li = criaLi()
+}
+
+/* -------------- Fim criaTarefa --------------*/
+
+
+
+
 btnTarefa.addEventListener(`click`, function(){
-    console.log(inputTarefas.value)
+    if (!inputTarefas.value) return;  // (lembrando) if com uma linha so nao precisa da chaves{}
+    criaTarefa(inputTarefas.value);
+    
 });
